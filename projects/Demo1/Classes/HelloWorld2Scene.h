2f119f7528be246ef9e50ca819ae2a7b41d868f9
Aron <wpp_work@163.com>
2014-08-21 15:24:17 +0800

    base 2dx with dumoinfo in display frame status

---
+++ projects/Demo1/Classes/HelloWorld2Scene.h
@@ -0,0 +1,8 @@
+#pragma once
+class HelloWorld2Scene
+{
+public:
+	HelloWorld2Scene(void);
+	~HelloWorld2Scene(void);
+};
+
---
+++ projects/Demo1/proj.win32/HelloWorld2Scene.cpp
@@ -0,0 +1,11 @@
+#include "HelloWorld2Scene.h"
+
+
+HelloWorld2Scene::HelloWorld2Scene(void)
+{
+}
+
+
+HelloWorld2Scene::~HelloWorld2Scene(void)
+{
+}
